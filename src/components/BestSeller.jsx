515ca54724ee47bbb6e43494983f2b63c30e128f
SreenuGenji <152174167+SreenuGenji@users.noreply.github.com>
2025-02-25 15:16:47 +0530

    Update BestSeller.jsx

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -9,7 +9,7 @@ const BestSeller = () => {
 
 
     useEffect(()=>{
-        const bestProduct = products.filter((item)=>(item.bestSeller));
+        const bestProduct = products.filter((item)=>(item.bestseller));
         setBestSeller(bestProduct.slice(0,5))
     },[])
 
@@ -24,7 +24,7 @@ const BestSeller = () => {
 
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
             {
-                bestSeller.map((item)=>(
+                bestSeller.map((item,index)=>(
                     <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price} />
                 ))
             }
